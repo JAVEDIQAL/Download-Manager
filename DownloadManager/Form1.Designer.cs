60f6da8075cd8ebc20d0eb43117c3b82e417a52e
JAVED IQAL <engrjavediqbal837@yahoo.com>
2015-05-12 17:15:45 +0500

    first commit

---
+++ DownloadManager/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DownloadManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadURL = new System.Windows.Forms.TextBox();
+            this.savePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.downloadbtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // downloadURL
+            // 
+            this.downloadURL.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.downloadURL.Location = new System.Drawing.Point(141, 34);
+            this.downloadURL.Name = "downloadURL";
+            this.downloadURL.Size = new System.Drawing.Size(348, 20);
+            this.downloadURL.TabIndex = 0;
+            // 
+            // savePath
+            // 
+            this.savePath.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.savePath.Location = new System.Drawing.Point(113, 113);
+            this.savePath.Name = "savePath";
+            this.savePath.Size = new System.Drawing.Size(282, 20);
+            this.savePath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " Enter URL of the Website:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Path to Save to:";
+            // 
+            // downloadbtn
+            // 
+            this.downloadbtn.Location = new System.Drawing.Point(375, 149);
+            this.downloadbtn.Name = "downloadbtn";
+            this.downloadbtn.Size = new System.Drawing.Size(123, 23);
+            this.downloadbtn.TabIndex = 5;
+            this.downloadbtn.Text = "Download";
+            this.downloadbtn.UseVisualStyleBackColor = true;
+            this.downloadbtn.Click += new System.EventHandler(this.downloadbtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(25, 187);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(358, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Download Progress:";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "E:\\download";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 222);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.downloadbtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.savePath);
+            this.Controls.Add(this.downloadURL);
+            this.Name = "Form1";
+            this.Text = "Downloader by Javed";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox downloadURL;
+        private System.Windows.Forms.TextBox savePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button downloadbtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+
+        public System.EventHandler btnLoad_Click { get; set; }
+    }
+}
+
---
+++ DownloadManager/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DownloadManager
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+
+
+        private void downloadbtn_Click(object sender, EventArgs e)
+        {
+           
+            WebClient myWebClient = new WebClient();
+
+            //Declarations for string objects
+            string downloadURL, path;
+            //raw URL taken from user
+           downloadURL =  this.downloadURL.Text;
+            path = this.savePath.Text;
+          
+           
+           Uri tmp = new Uri(downloadURL);
+           string EndPathFileName = tmp.Segments.Last();
+           path = path + @"\" + EndPathFileName;
+
+           HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://archive.org/download/AbMeriNigahonMein_201408/Ab%20Meri%20Nigahon%20Mein.wmv");
+
+           // Set some reasonable limits on resources used by this request
+           request.MaximumAutomaticRedirections = 4;
+           request.MaximumResponseHeadersLength = 4;
+           // Set credentials to use for this request.
+           request.Credentials = CredentialCache.DefaultCredentials;
+           HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+           Console.WriteLine("Content length is {0}", response.ContentLength);
+           Console.WriteLine("Content type is {0}", response.ContentType);
+
+           // Get the stream associated with the response.
+           Stream receiveStream = response.GetResponseStream();
+
+           // Pipes the stream to a higher level stream reader with the required encoding format. 
+           StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+           Console.WriteLine("Response stream received.");
+           //  Console.WriteLine(readStream.ReadToEnd());
+           response.Close();
+           readStream.Close();
+           
+           //downloads file using async method
+          
+           myWebClient.DownloadFileAsync(tmp, path);
+
+           downloadbtn.Text = "Download Started";
+           downloadbtn.Enabled = false;
+
+           myWebClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+           myWebClient.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+
+
+     
+            
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+
+
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+
+            progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+
+        }
+
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            
+            
+            MessageBox.Show("Download Completed");
+
+        }
+
+       /* private void btnLoad_Click(object sender, EventArgs e)
+        {
+
+        }
+        */
+        
+
+      
+
+    
+     
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+       
+   
+
+          
